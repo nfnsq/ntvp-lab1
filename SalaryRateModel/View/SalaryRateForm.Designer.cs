30d00577cba4eff78c04982a04d48465d3e523bf
5831_sts <tatyias95@gmail.com>
2016-11-08 19:08:41 +0700

    Create WinForms for solution

---
+++ SalaryRateModel/View/SalaryRateForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace View
+{
+    partial class SalaryRateForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Remove);
+            this.groupBox1.Controls.Add(this.Add);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 237);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(248, 111);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(19, 149);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(73, 41);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add Pay";
+            this.Add.UseVisualStyleBackColor = true;
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(166, 149);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 41);
+            this.Remove.TabIndex = 2;
+            this.Remove.Text = "Remove Pay";
+            this.Remove.UseVisualStyleBackColor = true;
+            // 
+            // SalaryRateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SalaryRateForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
