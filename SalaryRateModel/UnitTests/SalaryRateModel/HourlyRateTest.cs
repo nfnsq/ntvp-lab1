8a23018d71dacaff07c89d1836e13a3af07e1168
5831_sts <tatyias95@gmail.com>
2016-12-18 21:13:13 +0700

    Added unit tests, don't know is it correct tests or not

---
+++ SalaryRateModel/UnitTests/SalaryRateModel/HourlyRateTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SalaryRateModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.SalaryRateModel
+{
+    [TestFixture]
+    class HourlyRateTest
+    {
+        [TestCase(1, 1, TestName = "Тест при значениях параметров равных 1")]
+        [TestCase(double.MinValue, double.MinValue,
+            TestName = "Тест при значениях параметров равных MinValue")]
+        [TestCase(double.MinValue - 1, double.MinValue - 1,
+            TestName = "Тест при значениях параметров равных MinValue - 1")]
+        [TestCase(double.MaxValue, double.MaxValue,
+            TestName = "Тест при значениях параметров равных MaxValue")]
+        [TestCase(double.MaxValue + 1, double.MaxValue + 1,
+            TestName = "Тест при значениях параметров равных MaxValue + 1")]
+        [TestCase(0, 0, TestName = "Тест при значениях параметров равных 0")]
+        [TestCase(double.NaN, double.NaN, TestName = "Тест при значениях параметров равных NaN")]
+        [TestCase(double.NegativeInfinity, double.NegativeInfinity,
+            TestName = "Тест при значениях параметров равных NegativeInfinity")]
+        [TestCase(double.PositiveInfinity, double.PositiveInfinity,
+            TestName = "Тест при значениях параметров равных PositiveInfinity")]
+        public void HourlyRate(double paidPerHour, double hourAmount)
+        {
+            HourlyRate pay = new HourlyRate(paidPerHour, hourAmount);
+
+            double result = pay.GetSummOfPay();
+
+            var res = paidPerHour * hourAmount;
+
+            Assert.AreEqual(result, res);
+        }
+    }
+}
