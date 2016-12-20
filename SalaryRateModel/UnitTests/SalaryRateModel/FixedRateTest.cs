d34c73cc4e3d4d5d6e63035c4fd6f45c9385889b
5831_sts <tatyias95@gmail.com>
2016-12-20 18:31:11 +0700

    edited tests

--- SalaryRateModel/UnitTests/SalaryRateModel/FixedRateTest.cs
+++ SalaryRateModel/UnitTests/SalaryRateModel/FixedRateTest.cs
@@ -11,28 +11,26 @@ namespace UnitTests.SalaryRateModel
     [TestFixture]
     class FixedRateTest
     {
-        [TestCase(1, 1, TestName = "Тест при значениях параметров равных 1")]
-        [TestCase(double.MinValue, double.MinValue,
+        [TestCase(1, 1, 0.05,TestName = "Тест при значениях параметров равных 1")]
+        [TestCase(double.MinValue, double.MinValue, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных MinValue")]
-        [TestCase(double.MinValue - 1, double.MinValue - 1,
+        [TestCase(double.MinValue - 1, double.MinValue - 1, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных MinValue - 1")]
-        [TestCase(double.MaxValue, double.MaxValue,
+        [TestCase(double.MaxValue, double.MaxValue, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных MaxValue")]
-        [TestCase(double.MaxValue + 1, double.MaxValue + 1,
+        [TestCase(double.MaxValue + 1, double.MaxValue + 1, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных MaxValue + 1")]
-        [TestCase(0, 0, TestName = "Тест при значениях параметров равных 0")]
-        [TestCase(double.NaN, double.NaN, TestName = "Тест при значениях параметров равных NaN")]
-        [TestCase(double.NegativeInfinity, double.NegativeInfinity,
+        [TestCase(0, 0, 0, TestName = "Тест при значениях параметров равных 0")]
+        [TestCase(double.NaN, double.NaN, double.NaN, TestName = "Тест при значениях параметров равных NaN")]
+        [TestCase(double.NegativeInfinity, double.NegativeInfinity, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных NegativeInfinity")]
-        [TestCase(double.PositiveInfinity, double.PositiveInfinity,
+        [TestCase(double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity,
             TestName = "Тест при значениях параметров равных PositiveInfinity")]
-        public void FixedRate(double salary, double amountDay)
+        public void FixedRate(double salary, double amountDay, double res)
         {
             FixedRate pay = new FixedRate(salary, amountDay);
             
             double result = pay.GetSummOfPay();
-
-            var res = salary / 20 * amountDay;
 
             Assert.AreEqual(result, res);
         }
--- SalaryRateModel/UnitTests/SalaryRateModel/VariableRateTest.cs
+++ SalaryRateModel/UnitTests/SalaryRateModel/VariableRateTest.cs
@@ -11,28 +11,26 @@ namespace UnitTests.SalaryRateModel
     [TestFixture]
     class VariableRateTest
     {
-        [TestCase(1, 1, 1, TestName="Тест при параметрах равных 1")]
-        [TestCase(double.MinValue, double.MinValue, double.MinValue,
+        [TestCase(1, 1, 1, 0.05, TestName="Тест при параметрах равных 1")]
+        [TestCase(double.MinValue, double.MinValue, double.MinValue, double.NegativeInfinity,
             TestName = "Тест при параметрах равных MinValue")]
-        [TestCase(double.MinValue - 1, double.MinValue - 1, double.MinValue - 1,
+        [TestCase(double.MinValue - 1, double.MinValue - 1, double.MinValue - 1, double.NegativeInfinity,
             TestName = "Тест при параметрах равных MinValue - 1")]
-        [TestCase(double.MaxValue, double.MaxValue, double.MaxValue,
+        [TestCase(double.MaxValue, double.MaxValue, double.MaxValue, double.PositiveInfinity,
             TestName = "Тест при параметрах равных MaxValue")]
-        [TestCase(double.MaxValue + 1, double.MaxValue + 1, double.MaxValue + 1,
+        [TestCase(double.MaxValue + 1, double.MaxValue + 1, double.MaxValue + 1, double.PositiveInfinity,
             TestName = "Тест при параметрах равных MaxValue + 1")]
-        [TestCase(0, 0, 0, TestName = "Тест при параметрах равных 0")]
-        [TestCase(double.NaN, double.NaN, double.NaN, TestName = "Тест при ставке равном NaN")]
-        [TestCase(double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity,
+        [TestCase(0, 0, 0, 0, TestName = "Тест при параметрах равных 0")]
+        [TestCase(double.NaN, double.NaN, double.NaN, double.NaN, TestName = "Тест при ставке равном NaN")]
+        [TestCase(double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity,
             TestName = "Тест при параметрах равных NegativeInfinity")]
-        [TestCase(double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity,
+        [TestCase(double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity,
             TestName = "Тест при параметрах равных PositiveInfinity")]
-        public void VariableRate(double salary, double amountDay, double rate)
+        public void VariableRate(double salary, double amountDay, double rate, double res)
         {
             VariableRate pay = new VariableRate(salary, amountDay, rate);
 
             double result = pay.GetSummOfPay();
-
-            var res = salary / 20 * amountDay * rate;
 
             Assert.AreEqual(result, res);
         }
